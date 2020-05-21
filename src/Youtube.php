3ca12baeae90d35845b2fcd339eea238a4bca3dc
Ethan Finlay <efinclarke1993@gmail.com>
2020-05-21 11:54:07 -0400

    cntd

--- src/Youtube.php
+++ src/Youtube.php
@@ -63,6 +63,7 @@ class Youtube {
 
 			// Looks through the captions and checks if any were not auto-generated
 			foreach ($items as $track ) {
+                print($track);
 				if ( $track->snippet->trackKind != 'ASR' ) {
 					return 2;
 				}
