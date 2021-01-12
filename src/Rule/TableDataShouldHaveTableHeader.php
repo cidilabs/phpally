bdc2719880dcaffb9726a1c701cf28d4ae989fd0
Chuck Crandall <chuck@webchuckweb.com>
2021-01-12 15:01:14 -0700

    Skip text nodes when looking for TH in a table.

--- src/Rule/TableDataShouldHaveTableHeader.php
+++ src/Rule/TableDataShouldHaveTableHeader.php
@@ -5,26 +5,29 @@ namespace CidiLabs\PhpAlly\Rule;
 use DOMElement;
 
 /**
-*  All data tables contain th elements.
-*  Data tables must have th elements while layout tables can not have th elements.
-*/
+ *  All data tables contain th elements.
+ *  Data tables must have th elements while layout tables can not have th elements.
+ */
 class TableDataShouldHaveTableHeader extends BaseRule
 {
-    public static $severity = self::SEVERITY_ERROR;
-    
-    public function id()
-    {
-        return self::class;
-    }
+	public static $severity = self::SEVERITY_ERROR;
 
-    public function check()
-    {
-        foreach ($this->getAllElements('table') as $table) {
+	public function id()
+	{
+		return self::class;
+	}
+
+	public function check()
+	{
+		foreach ($this->getAllElements('table') as $table) {
 			foreach ($table->childNodes as $child) {
 				if ($this->propertyIsEqual($child, 'tagName', 'tbody') || $this->propertyIsEqual($child, 'tagName', 'thead')) {
 					foreach ($child->childNodes as $tr) {
 						if (!is_null($tr->childNodes)) {
 							foreach ($tr->childNodes as $th) {
+								if ($th->nodeType === XML_TEXT_NODE) {
+									continue;
+								}
 								if ($this->propertyIsEqual($th, 'tagName', 'th')) {
 									break 3;
 								} else {
@@ -46,12 +49,12 @@ class TableDataShouldHaveTableHeader extends BaseRule
 				}
 			}
 		}
-        
-        return count($this->issues);
-    }
 
-    // public function getPreviewElement(DOMElement $a = null)
-    // {
-    //     return $a->parentNode;
-    // }
+		return count($this->issues);
+	}
+
+	// public function getPreviewElement(DOMElement $a = null)
+	// {
+	//     return $a->parentNode;
+	// }
 }
