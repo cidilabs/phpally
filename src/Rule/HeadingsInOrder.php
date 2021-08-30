dcaa8da45f8256e775e546735fcee184175770e0
AlanFCMV <verdu.alan@gmail.com>
2021-08-30 16:21:11 -0400

    Add HeadingsInOrder rule

--- src/Rule/HeadingsInOrder.php
+++ src/Rule/HeadingsInOrder.php
@@ -3,9 +3,10 @@
 namespace CidiLabs\PhpAlly\Rule;
 
 use DOMElement;
+use DOMXPath;
 
 /**
-* 
+*
 *  CUSTOM TEST FOR UDOIT
 *  Checks if content is using the proper heading hierarchy
 */
@@ -18,7 +19,25 @@ class HeadingsInOrder extends BaseRule
 
     public function check()
     {
-        
+        $xpath = new DOMXPath($this->dom);
+        $headings = $xpath->query('//h1 | //h2 | //h3 | //h4 | //h5 | //h6');
+
+        // Check that we dont skip heading levels.
+        for ($i = 0; $i < count($headings); $i++) {
+            $currentLevel = (int)substr($headings[$i]->nodeName, -1);
+
+            // Check that we start with the right heading.
+            if ($i === 0) {
+                if ($currentLevel !== 1 && $currentLevel !== 2) {
+                    $this->setIssue($headings[$i]);
+                }
+            } elseif ($currentLevel > ($previousLevel + 1)) {
+                $this->setIssue($headings[$i]);
+            }
+
+            $previousLevel = $currentLevel;
+        }
+
         return count($this->issues);
     }
 
---
+++ tests/HeadingsInOrderTest.php
@@ -0,0 +1,136 @@
+<?php
+
+use CidiLabs\PhpAlly\Rule\HeadingsInOrder;
+
+class HeadingsInOrderTest extends PhpAllyTestCase {
+
+    public function testNoHeaders()
+    {
+        $html = '<p></p>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(0, $rule->check(), 'HeadersInOrder should have no issues.');
+    }
+
+    public function testSingleH1()
+    {
+        $html = '<h1></h1>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(0, $rule->check(), 'HeadersInOrder should have no issues.');
+    }
+
+    public function testSingleH2()
+    {
+        $html = '<h2></h2>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(0, $rule->check(), 'HeadersInOrder should have no issues.');
+    }
+
+    public function testSingleH3()
+    {
+        $html = '<h3></h3>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(1, $rule->check(), 'HeadersInOrder should have one issue.');
+    }
+
+    public function testSingleH4()
+    {
+        $html = '<h4></h4>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(1, $rule->check(), 'HeadersInOrder should have one issue.');
+    }
+
+    public function testSingleH5()
+    {
+        $html = '<h5></h5>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(1, $rule->check(), 'HeadersInOrder should have one issue.');
+    }
+
+    public function testSingleH6()
+    {
+        $html = '<h6></h6>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(1, $rule->check(), 'HeadersInOrder should have one issue.');
+    }
+
+    public function testSkipH2()
+    {
+        $html = '<h1></h1><h3></h3>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(1, $rule->check(), 'HeadersInOrder should have one issue.');
+    }
+
+    public function testSkipH3()
+    {
+        $html = '<h2></h2><h4></h4>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(1, $rule->check(), 'HeadersInOrder should have one issue.');
+    }
+
+    public function testSkipTwoLevels()
+    {
+        $html = '<h2></h2><h5></h5>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(1, $rule->check(), 'HeadersInOrder should have one issue.');
+    }
+
+    public function testSkipLevelsTwice()
+    {
+        $html = '<h2></h2><h4></h4><h6></h6>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(2, $rule->check(), 'HeadersInOrder should have two issues.');
+    }
+
+    public function testMaintainLevel()
+    {
+        $html = '<h1></h1></h2><h2></h2>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(0, $rule->check(), 'HeadersInOrder should have no issues.');
+    }
+
+    public function testDecreaseLevel()
+    {
+        $html = '<h1></h1><h2></h2><h1></h1>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new HeadingsInOrder($dom);
+
+        $this->assertEquals(0, $rule->check(), 'HeadersInOrder should have no issues.');
+    }
+}
