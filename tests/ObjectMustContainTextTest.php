e5b031e02a9f91edfe65d28bbc643b99a1b6723e
Ethan  Finlay <ethanfinlay@MacBook-Pro.attlocal.net>
2021-10-06 14:57:09 -0400

    objectMustContainText now checks aria-label and aria-labelledby

--- tests/ObjectMustContainTextTest.php
+++ tests/ObjectMustContainTextTest.php
@@ -32,4 +32,24 @@ class ObjectMustContainTextTest extends PhpAllyTestCase {
 
         $this->assertEquals(1, $rule->check(), 'Object Must Contain Text should have one issue.');
     }
+
+    public function testCheckAriaLabel()
+    {
+        $html = '<div><object aria-label="label" type="application/pdf"data="/media/examples/In-CC0.pdf"width="250"height="200"></object></div>';;
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new ObjectMustContainText($dom);
+
+        $this->assertEquals(0, $rule->check(), 'Object Must Contain Text should have no issues');
+    }
+
+    public function testCheckAriaLabeledBy()
+    {
+        $html = '<div><object aria-labelledby="label" type="application/pdf"data="/media/examples/In-CC0.pdf"width="250"height="200"></object></div>';;
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new ObjectMustContainText($dom);
+
+        $this->assertEquals(0, $rule->check(), 'Object Must Contain Text should have no issues');
+    }
 }
