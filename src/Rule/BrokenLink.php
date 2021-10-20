21a018c44e5f8faa5adc8bd57aaf009256d25641
AlanFCMV <verdu.alan@gmail.com>
2021-10-20 16:18:44 -0400

    Add new split rules

---
+++ src/Rule/BrokenLink.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace CidiLabs\PhpAlly\Rule;
+
+use DOMElement;
+
+/**
+*  Links that are broken need to be removed or manually updated.
+*  Based on UDOIT 2.8.X https://github.com/ucfopen/UDOIT/blob/classic/lib/Udoit.php
+*  contributions by Emily Sachs
+*/
+class BrokenLink extends BaseRule
+{
+
+	public function id()
+	{
+		return self::class;
+	}
+
+	private function linkCheck($links) {
+		$curls = array();
+		$mcurl = curl_multi_init();
+		foreach (array_keys($links) as $i => $link) {
+			$curls[$i] = curl_init();
+			curl_setopt($curls[$i], CURLOPT_URL, $link);
+			curl_setopt($curls[$i], CURLOPT_HEADER, true);
+			curl_setopt($curls[$i], CURLOPT_NOBODY, true);
+			curl_setopt($curls[$i], CURLOPT_REFERER, true);
+			curl_setopt($curls[$i], CURLOPT_TIMEOUT, 2);
+			curl_setopt($curls[$i], CURLOPT_AUTOREFERER, true);
+			curl_setopt($curls[$i], CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curls[$i], CURLOPT_FOLLOWLOCATION, true);
+			curl_multi_add_handle($mcurl, $curls[$i]);
+		}
+		$running = null;
+		do {
+			curl_multi_exec($mcurl, $running);
+		} while ($running > 0);
+		foreach (array_keys($links) as $i => $link) {
+			$status = curl_getinfo($curls[$i], CURLINFO_HTTP_CODE);
+			// If the status is greater than or equal to 400 the link is broken.
+			if (400 >= $status) {
+				$this->setIssue($links[$link]);
+			}
+			curl_multi_remove_handle($mcurl, $curls[$i]);
+		}
+		curl_multi_close($mcurl);
+	}
+
+	public function check()
+	{
+		$links = array();
+		foreach ($this->getAllElements('a') as $a) {
+			$href = $a->getAttribute('href');
+			if ($href) {
+				$links[$href] = $a;
+			}
+		}
+		$this->linkCheck($links);
+
+		return count($this->issues);
+	}
+}
---
+++ tests/BrokenLinkTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use CidiLabs\PhpAlly\Rule\BrokenLink;
+
+class BrokenLinkTest extends PhpAllyTestCase {
+    public function testCheckValid()
+    {
+        $html = '<div><a href="www.google.com">I am a link.</a><div>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new BrokenLink($dom);
+
+        $this->assertEquals(0, $rule->check(), 'BrokenLink should have no issue.');
+    }
+
+    public function testCheckBroken400()
+    {
+        $html = '<div><a href="http://www.deadlinkcity.com/error-page.asp?e=400
+        ">I am a link.</a><div>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new BrokenLink($dom);
+
+        $this->assertEquals(1, $rule->check(), 'BrokenLink should have one issue.');
+    }
+
+    public function testCheckBroken404()
+    {
+        $html = '<div><a href="https://webaim.org/brokenlink">I am a link.</a><div>';
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($html);
+        $rule = new BrokenLink($dom);
+
+        $this->assertEquals(1, $rule->check(), 'BrokenLink should have one issue.');
+    }
+}
