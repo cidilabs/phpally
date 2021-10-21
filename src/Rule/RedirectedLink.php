390712b796c742a7d895781fac7a225d6e7ee8c3
AlanFCMV <verdu.alan@gmail.com>
2021-10-21 10:57:19 -0400

    Refactor to check for permanent redirects only

--- src/Rule/RedirectedLink.php
+++ src/Rule/RedirectedLink.php
@@ -27,9 +27,7 @@ class RedirectedLink extends BaseRule
 			curl_setopt($curls[$i], CURLOPT_NOBODY, true);
 			curl_setopt($curls[$i], CURLOPT_REFERER, true);
 			curl_setopt($curls[$i], CURLOPT_TIMEOUT, 2);
-			curl_setopt($curls[$i], CURLOPT_AUTOREFERER, true);
 			curl_setopt($curls[$i], CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($curls[$i], CURLOPT_FOLLOWLOCATION, true);
 			curl_multi_add_handle($mcurl, $curls[$i]);
 		}
 		$running = null;
@@ -37,16 +35,14 @@ class RedirectedLink extends BaseRule
 			curl_multi_exec($mcurl, $running);
 		} while ($running > 0);
 		foreach (array_keys($links) as $i => $link) {
-			$redirect = curl_getinfo($curls[$i], CURLINFO_EFFECTIVE_URL);
+			$redirect = curl_getinfo($curls[$i], CURLINFO_REDIRECT_URL);
 			$status = curl_getinfo($curls[$i], CURLINFO_RESPONSE_CODE);
-			if ((400 > $status) && ($link != $redirect)) {
-				// Redirected link (May be a Canvas link that is not actually redirected)
-				$parsed_link = parse_url($link);
-				$parsed_redirect = parse_url($redirect);
-				if ($parsed_link[host] !== $parsed_redirect[host] ||
-					$parsed_link[path] !== $parsed_redirect[path])
-					$this->setIssue($links[$link], null, json_encode(array('redirect_url' => $redirect)));
+
+			// Only permanent redirections are a problem
+			if ($status === 301 || $status === 308) {
+				$this->setIssue($links[$link], null, json_encode(array('redirect_url' => $redirect)));
 			}
+
 			curl_multi_remove_handle($mcurl, $curls[$i]);
 		}
 		curl_multi_close($mcurl);
