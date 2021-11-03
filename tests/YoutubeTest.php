dcc092d9bd07fcd56fb17b4569bfdfdaa31fd805
Chuck Crandall <chuck@webchuckweb.com>
2021-11-03 14:28:50 -0600

    Video scan cleanup. Moved response validation to getVideoData(). Updated tests.

--- tests/YoutubeTest.php
+++ tests/YoutubeTest.php
@@ -1,204 +1,166 @@
 <?php
 
 use CidiLabs\PhpAlly\Video\Youtube;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Response;
 
 class YoutubeTest extends PhpAllyTestCase {
 
     public function testCaptionsMissing()
     {
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": []
-        }';
-
         $youtube = new Youtube($client, 'en', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = [];
 
-        $this->assertEquals($youtube->captionsMissing($response), 0);
+        $this->assertEquals($youtube->captionsMissing($response), YouTube::YOUTUBE_FAIL);
     }
 
     public function testCaptionsMissingHasCaptions()
     {
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": [
-                {
-                    "snippet": {
-                        "trackKind": "asr",
-                        "language": "es"
-                    }
-                },
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "es-419"
-                    }
-                }
-            ]
-        }';
-
         $youtube = new Youtube($client, 'en', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = \json_decode('[
+            {
+                "snippet": {
+                    "trackKind": "asr",
+                    "language": "es"
+                }
+            },
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "es-419"
+                }
+            }
+        ]');
 
-        $this->assertEquals($youtube->captionsMissing($response), 2);
+        $this->assertEquals($youtube->captionsMissing($response), YouTube::YOUTUBE_SUCCESS);
     }
 
     public function testCaptionsLanguageFail(){
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": [
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "es"
-                    }
-                },
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "es-419"
-                    }
-                }
-            ]
-        }';
-
         $youtube = new Youtube($client, 'en', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = \json_decode('[
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "es"
+                }
+            },
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "es-419"
+                }
+            }
+        ]');
 
-        $this->assertEquals($youtube->captionsLanguage($response), 0);
+        $this->assertEquals($youtube->captionsLanguage($response), YouTube::YOUTUBE_FAIL);
     }
 
     public function testCaptionsLanguageSuccess(){
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": [
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "en"
-                    }
-                },
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "es-419"
-                    }
-                }
-            ]
-        }';
-
         $youtube = new Youtube($client, 'en', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = \json_decode('[
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "en"
+                }
+            },
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "es-419"
+                }
+            }
+        ]');
 
-        $this->assertEquals($youtube->captionsLanguage($response), 2);
+        $this->assertEquals($youtube->captionsLanguage($response), YouTube::YOUTUBE_SUCCESS);
     }
 
     public function testCaptionsLanguageEmpty(){
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": []
-        }';
-
         $youtube = new Youtube($client, 'en', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = [];
 
-        $this->assertEquals($youtube->captionsLanguage($response), 2);
+        $this->assertEquals($youtube->captionsLanguage($response), YouTube::YOUTUBE_SUCCESS);
     }
 
     public function testCaptionsNoLanguage(){
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": [
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "en"
-                    }
-                },
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "es-419"
-                    }
-                }
-            ]
-        }';
-
         $youtube = new Youtube($client, '', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = \json_decode('[
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "en"
+                }
+            },
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "es-419"
+                }
+            }
+        ]');
 
-        $this->assertEquals($youtube->captionsLanguage($response), 2);
+        $this->assertEquals($youtube->captionsLanguage($response), YouTube::YOUTUBE_SUCCESS);
     }
 
     public function testCaptionsNoLanguageFailure(){
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": [
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "de"
-                    }
-                },
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "es-419"
-                    }
-                }
-            ]
-        }';
-
         $youtube = new Youtube($client, '', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = \json_decode('[
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "de"
+                }
+            },
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "es-419"
+                }
+            }
+        ]');
 
-        $this->assertEquals($youtube->captionsLanguage($response), 0);
+        $this->assertEquals($youtube->captionsLanguage($response), YouTube::YOUTUBE_FAIL);
     }
 
     public function testCaptionsAutoGeneratedFailure(){
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": [
-                {
-                    "snippet": {
-                        "trackKind": "asr",
-                        "language": "en"
-                    }
-                }
-            ]
-        }';
-
         $youtube = new Youtube($client, 'en', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = \json_decode('[
+            {
+                "snippet": {
+                    "trackKind": "asr",
+                    "language": "en"
+                }
+            }
+        ]');
 
-        $this->assertEquals($youtube->captionsAutoGenerated($response), 0);
+        $this->assertEquals($youtube->captionsAutoGenerated($response), YouTube::YOUTUBE_FAIL);
     }
 
     public function testCaptionsAutoGeneratedSuccess(){
         $client = new \GuzzleHttp\Client(['http_errors' => false]);
-        $string = '{
-            "items": [
-                {
-                    "snippet": {
-                        "trackKind": "asr",
-                        "language": "ru"
-                    }
-                },
-                {
-                    "snippet": {
-                        "trackKind": "standard",
-                        "language": "es-419"
-                    }
-                }
-            ]
-        }';
-
         $youtube = new Youtube($client, 'en', 'testApiKey');
-        $response = new Response(200, ['Content-Type' => 'application/json'], $string);
+        $response = \json_decode('[
+            {
+                "snippet": {
+                    "trackKind": "asr",
+                    "language": "ru"
+                }
+            },
+            {
+                "snippet": {
+                    "trackKind": "standard",
+                    "language": "es-419"
+                }
+            }
+        ]');
 
-        $this->assertEquals($youtube->captionsAutoGenerated($response), 2);
+        $this->assertEquals($youtube->captionsAutoGenerated($response), YouTube::YOUTUBE_SUCCESS);
     }
 
 }
