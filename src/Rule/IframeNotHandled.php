b66170ad5a4de9cdea5d4f80719e0ce4df26f25b
AlanFCMV <verdu.alan@gmail.com>
2021-12-09 09:19:45 -0500

    Add IframeNotHAndled tests

--- src/Rule/IframeNotHandled.php
+++ src/Rule/IframeNotHandled.php
@@ -4,6 +4,21 @@ namespace CidiLabs\PhpAlly\Rule;
 
 class IframeNotHandled extends BaseRule
 {
+    private $regex = array(
+        '@youtube\.com/embed/([^"\&\? ]+)@i',
+        '@youtube\.com/v/([^"\&\? ]+)@i',
+        '@youtube\.com/watch\?v=([^"\&\? ]+)@i',
+        '@youtube\.com/\?v=([^"\&\? ]+)@i',
+        '@youtu\.be/([^"\&\? ]+)@i',
+        '@youtu\.be/v/([^"\&\? ]+)@i',
+        '@youtu\.be/watch\?v=([^"\&\? ]+)@i',
+        '@youtu\.be/\?v=([^"\&\? ]+)@i',
+        '@youtube-nocookie\.com/embed/([^"\&\? ]+)@i',
+        '@vimeo\.com/[^0-9]*([0-9]{7,9})@i',
+        '/(kaltura)/',
+        '/(dailymotion)/',
+    );
+
     public function id()
     {
         return self::class;
@@ -11,12 +26,21 @@ class IframeNotHandled extends BaseRule
 
     public function check()
     {
-        $search = '/(youtube|youtu.be|vimeo|kaltura|dailymotion)/';
-
         foreach ($this->getAllElements('iframe') as $iframe) {
-            if (!preg_match($search, $iframe->getAttribute('src'))) {
+            $matches = false;
+
+            foreach ($this->regex as $search) {
+                if (preg_match($search, trim($iframe->getAttribute('src')))) {
+                    $matches = true;
+                    break;
+                }
+            }
+
+            if (!$matches) {
                 $this->setIssue($iframe);
             }
         }
+
+        return count($this->issues);
     }
 }
